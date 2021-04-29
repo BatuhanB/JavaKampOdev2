9a11c59c72b42a053dd13fa703aa560e3a945325
Batuhan Balı <balibatuhan19@gmail.com>
2021-04-29 21:25:49 +0300

    .

--- KampOdev2/src/kampOdev2/InstructorManager.java
+++ KampOdev2/src/kampOdev2/InstructorManager.java
@@ -6,6 +6,6 @@ public class InstructorManager extends UserManager{
 		
 	}
 	public void addCourse(Instructor instructor) {
-		System.out.println(instructor.getFirstName() + " " + instructor.getLastName() + " adli kullanici " + instructor.getGivenLecture() + " dersini verdi.");
+		System.out.println(instructor.getFirstName() + " " + instructor.getLastName() + " adlı kullanıcı " + instructor.getGivenLecture() + " dersini verdi.");
 	}
 }
--- KampOdev2/src/kampOdev2/StudentManager.java
+++ KampOdev2/src/kampOdev2/StudentManager.java
@@ -3,9 +3,9 @@ package kampOdev2;
 public class StudentManager extends UserManager{
 	@Override
 	public void add(User user) {
-		
+		System.out.println(user.getFirstName() + " " + user.getLastName() + " adlı kullanıcı eklendi");
 	}
 	public void addCourse(Student student) {
-		System.out.println(student.getFirstName() + " " + student.getLastName() + " adli kullanici " + student.getTakenLecture() + " dersini aldı.");
+		System.out.println(student.getFirstName() + " " + student.getLastName() + " adlı kullanıcı" + student.getTakenLecture() + " dersini aldı.");
 	}
 }
